284a1b33c04ca152738f5d4dd541846eedd924d2
alex-machulski <aliaksandr.machulski@gmail.com>
2021-04-08 14:56:23 +0300

    update last commit

--- src/n2-features/f1-auth/a1-login/Login.tsx
+++ src/n2-features/f1-auth/a1-login/Login.tsx
@@ -1,7 +1,7 @@
 import {Field, Form, Formik, FormikHelpers} from "formik";
 import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import { Redirect } from "react-router-dom";
+import {Redirect} from "react-router-dom";
 import {getAuthUserData, login} from "../../../state/login-reducer";
 import {AppRootStateType} from "../../../state/store";
 import {PROFILE_PATH} from "../../../App";
@@ -20,52 +20,52 @@ export const Login = () => {
     useEffect(() => {
         dispatch(getAuthUserData())
         dispatch(clearRegisterStatusAC());
-    },[])
+    }, [])
 
     const formikOnSubmit = (values: Values, {setSubmitting}: FormikHelpers<Values>) => {
         dispatch(login(values.email, values.password, values.rememberMe))
-return
+        return
 
     }
-    if (isLoggedIn){
+    if (isLoggedIn) {
         return <Redirect to={PROFILE_PATH}/>
     }
 
     return (
         <div>
-                <h1>Login</h1>
-                <Formik
+            <h1>Login</h1>
+            <Formik
                 initialValues={{
-                email: "",
-                password: "",
-                rememberMe: false
-            }}
+                    email: "",
+                    password: "",
+                    rememberMe: false
+                }}
                 onSubmit={formikOnSubmit}
-                >
+            >
                 <Form>
-                <label htmlFor="email">Email</label>
-                <Field
-                id="email"
-                name="email"
-                placeholder="email"
-                type="email"
-                />
-                <label htmlFor="password">Password</label>
-                <Field
-                id="password"
-                name="password"
-                placeholder="password"
-                type="password"
-                />
-                <Field
-                id="rememberMe"
-                name="rememberMe"
-                type="checkbox"
-                />
+                    <label htmlFor="email">Email</label>
+                    <Field
+                        id="email"
+                        name="email"
+                        placeholder="email"
+                        type="email"
+                    />
+                    <label htmlFor="password">Password</label>
+                    <Field
+                        id="password"
+                        name="password"
+                        placeholder="password"
+                        type="password"
+                    />
+                    <Field
+                        id="rememberMe"
+                        name="rememberMe"
+                        type="checkbox"
+                    />
 
-                <button type="submit">Submit</button>
+                    <button type="submit">Submit</button>
                 </Form>
-                </Formik>
-            </div>
+            </Formik>
+        </div>
     )
 }
